878a89884c242dfae77b54ffe3bc409fc5b2f773
JangDongGun <rjs595959@github.com>
2022-05-15 02:06:36 +0900

    [F] redux-toolkit 적용하기

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,13 +2,17 @@ import '../styles/globals.css';
 import theme from '@/styles/theme';
 import { ThemeProvider } from 'styled-components';
 import type { AppProps } from 'next/app';
-import { appWithTranslation } from 'next-i18next'
+import { appWithTranslation } from 'next-i18next';
+import { store } from 'store';
+import { Provider } from 'react-redux';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <ThemeProvider theme={theme}>
-            <Component {...pageProps} />
-        </ThemeProvider>
+        <Provider store={store}>
+            <ThemeProvider theme={theme}>
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </Provider>
     );
 }
 
