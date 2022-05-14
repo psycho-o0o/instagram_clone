908e2bdfd9bb9a876fcc0119f9e001fd415bc817
JangDongGun <rjs595959@github.com>
2022-05-14 19:29:24 +0900

    [F] next-i18next 다국어 적용하기

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+import { i18n } from './next-i18nextconfig';
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    i18n,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React from 'react';
+import {GetStaticPropsContext, GetStaticPropsResult} from 'next';
+import Login from '@/features/login/Login';
+import styled from 'styled-components';
+import { SSRConfig } from 'next-i18next';
+import {serverSideTranslations} from 'next-i18next/serverSideTranslations'
+
+
+const StyledSection = styled.section`
+  min-height: 100vh;
+  overflow: hidden;
+  flex-grow: 1;
+`
 
 function App() {
-  return <div></div>;
+    return (
+        <StyledSection>
+            <Login />
+        </StyledSection>
+    );
 }
+
+export const getStaticProps = async ({ locale } : GetStaticPropsContext) : Promise<GetStaticPropsResult<SSRConfig>> => ({
+    props : {
+        ...await serverSideTranslations(locale || 'kr', ['login'])
+    }
+})
 
 export default App;
