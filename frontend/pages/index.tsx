1294328f3f08d1bed3a6ffe39031024a59e56ce9
JangDongGun <rjs595959@github.com>
2022-05-14 19:41:35 +0900

    [F] getStaticProps Type 추가

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import {GetStaticPropsContext, GetStaticPropsResult} from 'next';
+import { GetStaticPropsContext, GetStaticPropsResult } from 'next';
 import Login from '@/features/login/Login';
 import styled from 'styled-components';
 import { SSRConfig } from 'next-i18next';
-import {serverSideTranslations} from 'next-i18next/serverSideTranslations'
-
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 const StyledSection = styled.section`
-  min-height: 100vh;
-  overflow: hidden;
-  flex-grow: 1;
-`
+    min-height: 100vh;
+    overflow: hidden;
+    flex-grow: 1;
+`;
 
 function App() {
     return (
@@ -20,10 +19,12 @@ function App() {
     );
 }
 
-export const getStaticProps = async ({ locale } : GetStaticPropsContext) : Promise<GetStaticPropsResult<SSRConfig>> => ({
-    props : {
-        ...await serverSideTranslations(locale || 'kr', ['login'])
-    }
-})
+export const getStaticProps = async ({
+    locale,
+}: GetStaticPropsContext): Promise<GetStaticPropsResult<SSRConfig>> => ({
+    props: {
+        ...(await serverSideTranslations(locale || 'kr', ['login'])),
+    },
+});
 
 export default App;
