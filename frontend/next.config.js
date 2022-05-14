f6d03446f5de2d19ca25f2085f8cd8ea11a4182b
JangDongGun <rjs595959@github.com>
2022-05-15 02:35:05 +0900

    [F] common.js형식 require로 수정

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-import { i18n } from './next-i18nextconfig';
+const { i18n } = require('./next-i18next.config');
 const nextConfig = {
     reactStrictMode: true,
     i18n,
