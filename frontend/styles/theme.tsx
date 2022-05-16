8afea38f0b42ab7f061062f05893f2c507f928b0
JangDongGun <rjs595959@github.com>
2022-05-16 11:57:44 +0900

    [F] theme에 skyBlue 추가하기

--- frontend/styles/theme.tsx
+++ frontend/styles/theme.tsx
@@ -3,6 +3,7 @@ import { DefaultTheme } from "styled-components";
 const theme : DefaultTheme = {
     colors: {
         gray: 'rgb(142, 142, 142)',
+        skyBlue : 'rgb(0, 149, 246)',
     },
 };
 
