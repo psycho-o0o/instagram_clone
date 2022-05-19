b48ec4647a5c29cd5a42ff2d895897d9be6d9245
rjs595959 <rjs595959@naver.com>
2022-05-20 01:43:30 +0900

    [F] 자주 사용되는 버튼 분리

---
+++ frontend/fragments/Buttons.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Button1 = styled.button`
+    appearance: none;
+    background: 0 0;
+    cursor: pointer;
+    display: block;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+        Helvetica, Arial, sans-serif;
+    font-size: 14px;
+    font-weight: 600;
+    padding: 5px 9px;
+    text-align: center;
+    text-overflow: ellipsis;
+    text-transform: inherit;
+    user-select: none;
+    width: auto;
+`;
