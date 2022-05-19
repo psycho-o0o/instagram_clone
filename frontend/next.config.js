25d73014a9574e0ab538f7992b7c5713f1916d9c
rjs595959 <rjs595959@naver.com>
2022-05-20 03:25:38 +0900

    [F] 프록시 설정

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,16 @@ const { i18n } = require('./next-i18next.config');
 const nextConfig = {
     reactStrictMode: true,
     i18n,
+    async rewrites() {
+        if (process.env.NODE_ENV !== 'production') {
+            return [
+                {
+                    destination: process.env.DESTINATION_URL,
+                    source: process.env.SOURCE_PATH,
+                },
+            ];
+        }
+    },
 };
 
 module.exports = nextConfig;
