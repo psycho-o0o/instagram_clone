1dd734c3b16688b1269fe72307d79d8d95563de5
rjs595959 <rjs595959@naver.com>
2022-05-20 01:42:36 +0900

    [F] 필요없는 타입 제거

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,7 @@
-import React, { useCallback, useEffect } from 'react';
+import React from 'react';
 import {
-    GetServerSideProps,
     GetServerSidePropsContext,
     GetServerSidePropsResult,
-    GetStaticPropsContext,
-    GetStaticPropsResult,
 } from 'next';
 import { SSRConfig } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
@@ -44,7 +41,7 @@ export const getServerSideProps = async ({
             userAgent: req ? req.headers['user-agent'] : navigator.userAgent,
             ...(await serverSideTranslations(
                 locale || (defaultLocale as string),
-                ['main'],
+                ['main', 'login'],
                 nextI18NextConfig
             )),
         },
