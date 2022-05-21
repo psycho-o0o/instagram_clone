fdb673d25623c5d363a0d3175c77ca371a2c624e
JangDongGun <rjs595959@github.com>
2022-05-21 18:24:13 +0900

    [B] user의 로그인 라우트 구현 완료

--- backend/src/api/routes/users.routes.ts
+++ backend/src/api/routes/users.routes.ts
@@ -1,39 +1,75 @@
 import express, { NextFunction, Router, Request, Response } from 'express';
 import mongoose from 'mongoose';
-import UserSchema from '../db/model/user';
+import UserSchema, { IUserSchemaProps } from '../db/model/user';
+import { makeHashPassword, comparePassword } from '../../api/utils/user.utils';
 import { generateToken, validateToken } from '../../api/utils/jwt.utils';
 
+interface IRequestUserInfoProps {
+	phone?: string;
+	email?: string;
+	nickName?: string;
+}
+
 const router: Router = express.Router();
+
 router.post('/login', (req: Request, res: Response, next: NextFunction) => {
 	const { id, pw } = req.body;
-
 	if (id === undefined || pw === undefined) {
 		res.status(401); // Unauthorized
 	}
 
+	console.log(makeHashPassword(pw));
+
+	const emailRegExp = /[^@]+@.+/;
+	const phoneRegExp = /01[0-9]{9}/;
+	const requestUserInfo: IRequestUserInfoProps = {};
+
+	if (emailRegExp.test(id)) {
+		requestUserInfo.email = id;
+	} else if (phoneRegExp.test(id)) {
+		requestUserInfo.phone = id;
+	} else {
+		requestUserInfo.nickName = id;
+	}
+
 	const userModel = mongoose.model('User', UserSchema);
 
-	userModel.create({
-		phone: '01081389280',
-		password: 'evoDevs'
-	});
+	userModel.findOne(requestUserInfo, (err: any, userInfo: any) => {
+		if (err !== null) {
+			console.log(`err : ${err}`);
+			res.status(401).end(); // unauthorized
+			return;
+		}
 
-	userModel.findOne({ phone : '01081389280'}, (err: any, userInfo: any) => {
-		if(err !== null) {
-            console.log(`err : ${err}`);
-            res.status(401); // unauthorized
-            return;
-        } 
+		if (userInfo === null || userInfo === undefined) {
+			res.status(401)
+				.json({
+					error: 'non-existent id'
+				})
+				.end();
+			return;
+		}
 
-        if(userInfo === null || userInfo === undefined) {
-            res.status(401);
-        }
+		const payload = { ...userInfo._doc };
+		const { password } = payload;
 
-        const payload = {...userInfo._doc};
-        delete payload.password
-       
-        
-        
+		delete payload.password;
+
+		if (!comparePassword(pw, password)) {
+			res.status(401)
+				.json({
+					error: 'incorrect password'
+				})
+				.end();
+			return;
+		}
+
+		const jwt = generateToken(payload);
+		res.status(200)
+			.json({
+				jwt
+			})
+			.end();
 	});
 });
 
