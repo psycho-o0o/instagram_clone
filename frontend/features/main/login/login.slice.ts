3cbf5c4789c001979ba97ca06a881f9da62b0244
JangDongGun <rjs595959@github.com>
2022-05-21 20:02:43 +0900

    [F] api 파일 제거 및 rejectWithValue 추가

--- frontend/features/main/login/login.slice.ts
+++ frontend/features/main/login/login.slice.ts
@@ -1,20 +1,43 @@
+import axios from 'axios';
 import { createSlice, PayloadAction, createAsyncThunk } from '@reduxjs/toolkit';
-import { loginApi } from './login.api';
+
+interface ILoginApiProps {
+    id: string;
+    pw: string;
+}
+
+export const LoginThunk = createAsyncThunk(
+    'login/loginApi',
+    async (data: ILoginApiProps, { rejectWithValue }) => {
+        try {
+            const axioxConfig = {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            };
+            const response = await axios.post(
+                '/api/users/login',
+                data,
+                axioxConfig
+            );
+            return response?.data;
+        } catch (err: any) {
+            return rejectWithValue(err.response.data);
+        }
+    }
+);
 
 export interface LoginState {
     id: string;
     pw: string;
+    error: null | string;
 }
 
 const initialState: LoginState = {
     id: '',
     pw: '',
+    error: null,
 };
-
-export const LoginThunk = createAsyncThunk(
-    "login/loginApi",
-    loginApi
-)
 
 interface IChangeInputPayload {
     key: 'id' | 'pw';
@@ -30,10 +53,14 @@ export const loginSlice = createSlice({
         },
     },
     extraReducers: (builder) => {
-        builder.addCase(LoginThunk.fulfilled, (state, action) => {
-            console.log(action);
-        })
-    }
+        builder
+            .addCase(LoginThunk.fulfilled, (state, { payload: { jwt } }) => {
+                console.log(jwt);
+            })
+            .addCase(LoginThunk.rejected, (state, { payload }) => {
+                console.log(payload);
+            });
+    },
 });
 
 // Action creators are generated for each case reducer function
