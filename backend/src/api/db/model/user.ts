7e95265d464cab18d32cd9a26842c0d21671d8f2
JangDongGun <rjs595959@github.com>
2022-05-21 18:23:15 +0900

    [B] UserSchem의 Type 정의

--- backend/src/api/db/model/user.ts
+++ backend/src/api/db/model/user.ts
@@ -1,13 +1,22 @@
 import { Schema } from 'mongoose';
 
+export interface IUserSchemaProps {
+	_id : Schema.Types.ObjectId,
+	phone? : string,
+	email? : string,
+	name : string,
+	nickName : string,
+	password : string,
+}
+
 // 참고 : https://github.com/Zwimber/mongoose-style-guide
-const UserSchema = new Schema({
-	__id: Schema.Types.ObjectId,
-	phone: String,
-	email: String,
-	name: String,
-	nickName: String,
-	password: String
+const UserSchema = new Schema<IUserSchemaProps>({
+	_id: Schema.Types.ObjectId,
+	phone: {type : String, unique: true},
+	email: {type : String, unique : true},
+	name: {type : String, required: true},
+	nickName: {type : String, required: true, unique : true},
+	password: {type : String, required : true, unique : true}
 });
 
 UserSchema.pre('save', function (next) {
