9d485f0b3e7cdd07b835632024ff89b5383292a5
JangDongGun <rjs595959@github.com>
2022-05-21 18:25:11 +0900

    [B] hashPassword와 comparePassword 만들기

---
+++ backend/src/api/utils/user.utils.ts
@@ -0,0 +1,13 @@
+import bcrypt from 'bcrypt';
+
+const makeHashPassword = (password: string) => {
+	const hashedPassword = bcrypt.hashSync(password, process.env.SALT as string);
+
+	return hashedPassword;
+};
+
+const comparePassword = (password: string, HashedPassword: string) => {
+	return bcrypt.compareSync(password, HashedPassword);
+};
+
+export { makeHashPassword, comparePassword };
