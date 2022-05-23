9544798d829ea7e2c2edb31294ee099ba29caa56
rjs595959 <rjs595959@naver.com>
2022-05-23 19:05:44 +0900

    [B] payload에서 password 항목 지우기 위하여 타입 수정

--- backend/src/api/routes/users.routes.ts
+++ backend/src/api/routes/users.routes.ts
@@ -2,7 +2,8 @@ import express, { NextFunction, Router, Request, Response } from 'express';
 import mongoose from 'mongoose';
 import UserSchema, { IUserSchemaProps } from '../db/model/user';
 import { makeHashPassword, comparePassword } from '../../api/utils/user.utils';
-import { generateToken, validateToken } from '../../api/utils/jwt.utils';
+import { generateToken } from '../../api/utils/jwt.utils';
+import e from 'express';
 
 interface IRequestUserInfoProps {
 	phone?: string;
@@ -10,15 +11,22 @@ interface IRequestUserInfoProps {
 	nickName?: string;
 }
 
+interface ISaveUserInfoProps {
+	phone?: string;
+	email?: string;
+	name: string;
+	nickName: string;
+	password: string;
+}
+
 const router: Router = express.Router();
 
 router.post('/login', (req: Request, res: Response, next: NextFunction) => {
 	const { id, pw } = req.body;
 	if (id === undefined || pw === undefined) {
-		res.status(401); // Unauthorized
+		res.status(401).send({ message: 'not entered input' }); // Unauthorized
+		return;
 	}
-
-	console.log(makeHashPassword(pw));
 
 	const emailRegExp = /[^@]+@.+/;
 	const phoneRegExp = /01[0-9]{9}/;
@@ -33,20 +41,19 @@ router.post('/login', (req: Request, res: Response, next: NextFunction) => {
 	}
 
 	const userModel = mongoose.model('User', UserSchema);
-
 	userModel.findOne(requestUserInfo, (err: any, userInfo: any) => {
 		if (err !== null) {
 			console.log(`err : ${err}`);
-			res.status(401).end(); // unauthorized
+			res.status(401).send({
+				message: 'unkown error'
+			}); // unauthorized
 			return;
 		}
 
 		if (userInfo === null || userInfo === undefined) {
-			res.status(401)
-				.json({
-					error: 'non-existent id'
-				})
-				.end();
+			res.status(401).send({
+				message: 'non-existent id'
+			});
 			return;
 		}
 
@@ -56,21 +63,74 @@ router.post('/login', (req: Request, res: Response, next: NextFunction) => {
 		delete payload.password;
 
 		if (!comparePassword(pw, password)) {
-			res.status(401)
-				.json({
-					error: 'incorrect password'
-				})
-				.end();
+			res.status(401).send({
+				message: 'incorrect password'
+			});
 			return;
 		}
 
 		const jwt = generateToken(payload);
-		res.status(200)
-			.json({
-				jwt
-			})
-			.end();
+		res.status(200).send({
+			jwt
+		});
 	});
 });
+
+router.post(
+	'/register',
+	async (req: Request, res: Response, next: NextFunction) => {
+		const { id, name, nickName, pw } = req.body;
+		if ([id, name, nickName, pw].includes(undefined)) {
+			res.status(401).send({ message: 'not entered input' }); // Unauthorized
+			return;
+		}
+
+		let idType: 'email' | 'phone' | undefined = undefined;
+		const emailRegExp = /[^@]+@.+/;
+		const phoneRegExp = /01[0-9]{9}/;
+		const userInfo: ISaveUserInfoProps = {
+			name,
+			nickName,
+			password: makeHashPassword(pw)
+		};
+
+		if (emailRegExp.test(id)) {
+			idType = 'email';
+			userInfo.email = id;
+		} else if (phoneRegExp.test(id)) {
+			idType = 'phone';
+			userInfo.phone = id;
+		} else {
+			res.status(401).send({ message: 'incorrect id' });
+			return;
+		}
+
+		const userModel = mongoose.model('User', UserSchema);
+		try {
+			const isPresent =
+				(await userModel.exists({ idType: userInfo[idType] })) !== null;
+			if (isPresent) {
+				res.status(401).send({
+					message: 'present user'
+				});
+				return;
+			} else {
+				const doc = new userModel(userInfo);
+				const payload = (await doc.save()).toObject();
+				delete payload.password;
+
+				const jwt = generateToken(payload);
+				res.status(200).send({
+					jwt
+				});
+			}
+		} catch (e) {
+			console.log(e);
+			res.status(401).send({
+				message: 'unkown error'
+			});
+		}
+	}
+);
 
 export default router;
