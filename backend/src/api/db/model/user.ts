663ca03cd2b88cc7298ca52e97e8a7e1a7593f36
rjs595959 <rjs595959@naver.com>
2022-05-25 17:12:43 +0900

    fix user에 대한 정보 수정 및 email과 phone null 적용

--- backend/src/api/db/model/user.ts
+++ backend/src/api/db/model/user.ts
@@ -1,20 +1,26 @@
 import { Schema } from 'mongoose';
 
 export interface IUserSchemaProps {
-	phone? : string,
-	email? : string,
-	name : string,
-	nickName : string,
-	password? : string,
+	phone?: string;
+	email?: string;
+	name: string;
+	nickName: string;
+	password?: string;
 }
 
 // 참고 : https://github.com/Zwimber/mongoose-style-guide
 const UserSchema = new Schema<IUserSchemaProps>({
-	phone: {type : String, unique: true},
-	email: {type : String, unique : true},
-	name: {type : String, required: true},
-	nickName: {type : String, required: true, unique : true},
-	password: {type : String, required : true, unique : true}
+	phone: {
+		type: String,
+		index: { unique: true, sparse: true }
+	},
+	email: {
+		type: String,
+		index: { unique: true, sparse: true }
+	},
+	name: { type: String, required: true },
+	nickName: { type: String, required: true, unique: true },
+	password: { type: String, required: true, unique : false }
 });
 
 UserSchema.pre('save', function (next) {
