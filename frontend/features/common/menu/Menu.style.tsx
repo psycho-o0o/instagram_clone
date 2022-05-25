4a194f49d511b12615b273b358de6bdc3a498bd7
rjs595959 <rjs595959@naver.com>
2022-05-25 21:24:14 +0900

    feat 하단 메뉴 완성

---
+++ frontend/features/common/menu/Menu.style.tsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const FooterWrapper = styled.footer`
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom : 0;
+    height: 44px;
+`;
+
+export const FooterWrap = styled.div`
+    height: 100%;
+    display: flex;
+    flex-direction : row;
+    justify-content : space-between;
+`
+
+export const MenuWrapper = styled.div`
+    min-height: 0;
+    min-width: 0;
+    flex : 1 1 auto;
+    height: 100%;
+    justify-content : center;
+    align-items : center;
+`
---
+++ frontend/features/common/menu/Menu.tsx
@@ -0,0 +1,80 @@
+import React, { useEffect } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { FooterWrapper, FooterWrap, MenuWrapper } from './Menu.style';
+
+function Menu() {
+    const router = useRouter();
+    const baseIconsUrl = '/images/icon';
+
+    return (
+        <FooterWrapper>
+            <FooterWrap>
+                <MenuWrapper>
+                    <Link href="#home">
+                        <Image
+                            src={
+                                router.pathname === '/'
+                                    ? `${baseIconsUrl}/home_selected.svg`
+                                    : `${baseIconsUrl}/home.svg`
+                            }
+                            width={24}
+                            height={24}
+                        />
+                    </Link>
+                </MenuWrapper>
+                <MenuWrapper>
+                    <Link href="explore">
+                        <Image
+                            src={
+                                router.pathname === '/explore'
+                                    ? `${baseIconsUrl}/explore_selected.svg`
+                                    : `${baseIconsUrl}/explore.svg`
+                            }
+                            width={24}
+                            height={24}
+                        />
+                    </Link>
+                </MenuWrapper>
+                <MenuWrapper>
+                    <Link href="upload">
+                        <Image
+                            src="/images/icon/newPost.svg"
+                            width={24}
+                            height={24}
+                        />
+                    </Link>
+                </MenuWrapper>
+                <MenuWrapper>
+                    <Link href="activity">
+                        <Image
+                            src={
+                                router.pathname === '/activity'
+                                    ? `${baseIconsUrl}/activity_selected.svg`
+                                    : `${baseIconsUrl}/activity.svg`
+                            }
+                            width={24}
+                            height={24}
+                        />
+                    </Link>
+                </MenuWrapper>
+                <MenuWrapper>
+                    <Link href="profile">
+                        <Image
+                            src={
+                                router.pathname === '/profile'
+                                    ? `${baseIconsUrl}${router.pathname}_selected.svg`
+                                    : `${baseIconsUrl}${router.pathname}.svg`
+                            }
+                            width={24}
+                            height={24}
+                        />
+                    </Link>
+                </MenuWrapper>
+            </FooterWrap>
+        </FooterWrapper>
+    );
+}
+
+export default Menu;
