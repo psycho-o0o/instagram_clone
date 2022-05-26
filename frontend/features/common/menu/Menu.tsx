c2fc46cba34b8c20d72430ba15973b5c0a8fab3c
rjs595959 <rjs595959@naver.com>
2022-05-26 22:34:59 +0900

    @style max-width 600px로 고정시키기

--- frontend/features/common/menu/Menu.tsx
+++ frontend/features/common/menu/Menu.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-import { FooterWrapper, FooterWrap, MenuWrapper, FooterEmptyWrap } from './Menu.style';
+import { FooterWrapper, FooterWrap, MenuWrapper } from './Menu.style';
 
 function Menu() {
     const router = useRouter();
--- frontend/features/home/Home.style.tsx
+++ frontend/features/home/Home.style.tsx
@@ -1,49 +1,58 @@
-import styled, { DefaultTheme } from 'styled-components'
+import styled, { DefaultTheme } from 'styled-components';
 
-export const HeaderWrapper = styled.header`  
+export const Wrapper = styled.div`
+    width: 600px;
+    margin: 0 auto;
+    @media screen and (max-width: 600px) {
+        width: 100%;
+        margin : 0;
+    }   
+`;
+
+export const HeaderWrapper = styled.header`
     position: sticky;
-    width : 100%;
-    height : 44px;
-    display : flex;
-`
+    width: 100%;
+    height: 44px;
+    display: flex;
+`;
 export const HeaderWrap = styled.div`
     display: flex;
     flex-direction: row;
-    padding : 0 16px;
-    width : 100%;
+    padding: 0 16px;
+    width: 100%;
     height: 100%;
-`
+`;
 export const CameraButtonWrapper = styled.div`
-    flex : 0 0 32px;
-    justify-content : center;
-`
+    flex: 0 0 32px;
+    justify-content: center;
+`;
 export const CameraButton = styled.button`
-   background : 0 0;
-   border : 0;
-   margin : 0;
-   padding : 0; 
-`
+    background: 0 0;
+    border: 0;
+    margin: 0;
+    padding: 0;
+`;
 export const InstagramLogoWrapper = styled.div`
-    flex : 1;
-    justify-content : center;
+    flex: 1;
+    justify-content: center;
     align-items: center;
 `;
 export const InstagramLogoWrap = styled.div`
     margin-top: 8px;
 `;
 export const SendMessageWrapper = styled.div`
-    flex : 0 0 32px;
+    flex: 0 0 32px;
     margin-bottom: 5px;
-    justify-content : center;
-`
+    justify-content: center;
+`;
 export const StoryWrapper = styled.div<DefaultTheme>`
-    background-color: ${props => props.colors.gray7};
-    border-top: 1px solid ${props => props.colors.gray4};
-    border-bottom: 1px solid ${props => props.colors.gray4};
+    background-color: ${(props) => props.colors.gray7};
+    border-top: 1px solid ${(props) => props.colors.gray4};
+    border-bottom: 1px solid ${(props) => props.colors.gray4};
     padding: 10px 0;
-`
+    max-width: 600px;
+`;
 export const StoryWrap = styled.div`
     height: 85px;
-`
-export const FeedWrapper = styled.div``
-
+`;
+export const FeedWrapper = styled.div``;
