44744948307d5a5e798814dde979bcc2504fe48f
rjs595959 <rjs595959@naver.com>
2022-05-27 17:38:47 +0900

    @fix eslint 오류 수정

--- frontend/pages/home.tsx
+++ frontend/pages/home.tsx
@@ -1,22 +1,18 @@
 import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import { useAppDispatch, useAppSelector } from '@/redux/hooks';
+import { useAppSelector } from '@/redux/hooks';
 import Home from '@/features/home/Home';
 
-
 export default function HomePage() {
-    const router = useRouter();
-    const dispatch = useAppDispatch();
-    const { isLogin } = useAppSelector((state) => ({
-        isLogin: state.user.isLogin,
-    }));
+  const router = useRouter();
+  const { isLogin } = useAppSelector((state) => ({
+    isLogin: state.user.isLogin,
+  }));
 
-    useEffect(() => {
-        if(!isLogin) router.push('/');
-    }, [isLogin])
+  useEffect(() => {
+    if (!isLogin) router.push('/');
+  }, [isLogin]);
 
-    if(isLogin) return <Home />;
-    else {
-        return <div></div>;
-    }
+  if (isLogin) return <Home />;
+  return <div />;
 }
