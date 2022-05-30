cbb1408ed2c473ea9f0bdc3fbef23b0ba8b7c914
rjs595959 <rjs595959@naver.com>
2022-05-30 23:41:14 +0900

    @refactor Register 리팩토링

--- frontend/features/main/register/register.interface.ts
+++ frontend/features/main/register/register.interface.ts
@@ -3,6 +3,23 @@ export interface IRegisterProps {
     onClickLogIn : () => void
 }
 
+// Register.style.tsx
+export interface IRegisterInputLabel {
+    isEmptyInput: boolean;
+}
+
+export interface IToggleWrap {
+    isEmpty?: boolean;
+}
+
+export interface IRegisterButton {
+    isPossibleRegister: boolean;
+}
+
+export interface IErrorWrapperProps {
+    hide : boolean;
+}
+
 // register.slice.ts
 export interface IRegisterState {
     id: string;
